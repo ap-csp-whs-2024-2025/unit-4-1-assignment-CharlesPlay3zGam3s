f1cfc09db2d9e9ba535480e584c84577e1f19dbd
Mr. Charles <94595457+CharlesPlay3zGam3s@users.noreply.github.com>
2024-12-11 21:03:51 +0000

    Learned about Cout (output) and Endl New

--- classwork.cpp
+++ classwork.cpp
@@ -8,6 +8,9 @@ int main()
 double price = 19.99; 
 bool isRaining = false; 
 
-std::cout << age << "\n" <<price << std::endl << isRaining;
+std::cout << age << "\n" <<price << "\n" << isRaining << std::endl; //cout allows to
+
+int grade_level = 12;
+ std::cout << "I am a " << grade_level << "th grader." << std::endl;
   return 0;
 }
