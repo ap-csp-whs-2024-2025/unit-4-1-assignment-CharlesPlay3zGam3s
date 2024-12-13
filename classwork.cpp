c3631825dc37ffbd55c08aa56f711a9e5dc7178e
Mr. Charles <94595457+CharlesPlay3zGam3s@users.noreply.github.com>
2024-12-13 21:32:06 +0000

    Exercise 1 (I did it)

--- classwork.cpp
+++ classwork.cpp
@@ -3,31 +3,45 @@
 
 int main()
 {
-  int x;
-  int y;
-  int add_xy;
-  int sub_xy;
-  int mult_xy;
-  int div_xy
-  int mod_xy;
 
-  x = 25;
-  y = 5;
-add_xy = x+y;
-sub_xy = x - y;
-std::cout << add_xy << std::endl
-          << sub_xy << std::endl
-          <<mult_xy << std::endl
-          <<div_xy << std::endl
-          <<mod_xy << std:endl;
+  //E Exercise 1
 
-  //Exercise 2
-  bool isSunny = true;
-  bool hasUmbrella = false;
-  std::cout << (iSunny && hasUmbrella) << std::endl;
+   int x;
+   int y;
+   int z;
+   x = 25;
+   y = 3;
+   z = 10;
 
-  bool sun_or_umb = isSunny || hasUmbrella;
-  std::cout << sun_or_umb << "\n";
+   int add_xy;
+   int sub_xy;
+   int mult_xy;
+   int div_xy;
+   int mod_xy;
+   bool more_xz;
+   bool less_yx;
 
+   add_xy = x + y; 
+   sub_xy = x - y;
+   mult_xy = x * y;
+   div_xy = x / y;
+   mod_xy = x % y;
+   more_xz = x > z;
+   less_yx = y < x;
+
+   std::cout << add_xy << std::endl;
+   std::cout << sub_xy << std::endl;
+   std::cout << mult_xy << std::endl;
+   std::cout << div_xy << std::endl;
+  std::cout << mod_xy << std::endl;
+  std::cout << more_xz << std::endl;
+  std::cout << less_yx << std::endl;
+  // Exercise 6
+
+ //std::string x;
+ //std::cout << "What is your name?" << std::endl;
+ //std:: cin >> x;
+
+ //std::cout << "Hello " << x << ". It is nice to meet you!" << std::endl;
   return 0;
 }
