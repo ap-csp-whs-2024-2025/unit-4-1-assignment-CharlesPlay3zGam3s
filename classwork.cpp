05cac046a86f39363cff2cabfe210bf49f5f511d
Mr. Charles <94595457+CharlesPlay3zGam3s@users.noreply.github.com>
2024-12-13 21:42:53 +0000

    I learned more in Exercise 2

--- classwork.cpp
+++ classwork.cpp
@@ -6,36 +6,45 @@ int main()
 
   //E Exercise 1
 
-   int x;
-   int y;
-   int z;
-   x = 25;
-   y = 3;
-   z = 10;
+   //int x;
+   //int y;
+   //int z;
+  // x = 25;
+  // y = 3;
+  // z = 10;
 
-   int add_xy;
-   int sub_xy;
-   int mult_xy;
-   int div_xy;
-   int mod_xy;
-   bool more_xz;
-   bool less_yx;
+  // int add_xy;
+  // int sub_xy;
+  // int mult_xy;
+ //  int div_xy;
+  // int mod_xy;
+  // bool more_xz;
+  // bool less_yx;
 
-   add_xy = x + y; 
-   sub_xy = x - y;
-   mult_xy = x * y;
-   div_xy = x / y;
-   mod_xy = x % y;
-   more_xz = x > z;
-   less_yx = y < x;
+  // add_xy = x + y; 
+  // sub_xy = x - y;
+  // mult_xy = x * y;
+  // div_xy = x / y;
+  // mod_xy = x % y;
+  // more_xz = x > z;
+  // less_yx = y < x;
 
-   std::cout << add_xy << std::endl;
-   std::cout << sub_xy << std::endl;
-   std::cout << mult_xy << std::endl;
-   std::cout << div_xy << std::endl;
-  std::cout << mod_xy << std::endl;
-  std::cout << more_xz << std::endl;
-  std::cout << less_yx << std::endl;
+  // std::cout << add_xy << std::endl;
+  // std::cout << sub_xy << std::endl;
+  // std::cout << mult_xy << std::endl;
+  // std::cout << div_xy << std::endl;
+//  std::cout << mod_xy << std::endl;
+ // std::cout << more_xz << std::endl;
+ // std::cout << less_yx << std::endl;
+
+ //Exercise 2
+ bool isSunny = true;
+bool hasUmbrella = false;
+std::cout << (isSunny && hasUmbrella) << std ::endl;
+
+bool sun_or_umb = isSunny || hasUmbrella;
+std::cout << sun_or_umb << "\n";
+
   // Exercise 6
 
  //std::string x;
